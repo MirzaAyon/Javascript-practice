17ef57072ecea7b064199640e946dc4933008c00
MirzaAyon <ayonmirza98@gmail.com>
2022-05-16 13:53:06 +0600

    object added

---
+++ object-and-array.js
@@ -0,0 +1,20 @@
+//object with array
+var student1 = {
+    firstName: "Mirza",
+    lastName: "Ayon",
+    age: 23,
+    married: false,
+};
+console.log(student1);
+
+//object with array
+var student2 = {
+    firstName: "Mirza",
+    lastName: "Ayon",
+    age: 23,
+    hobby: ["h1", "h2"],
+    dinner: false
+};
+console.log(student2);
+
+//array
