d30eaa1486e0fb30345e9d50c5f39f4a92b7f00d
MirzaAyon <ayonmirza98@gmail.com>
2022-05-16 13:39:30 +0600

    number string boolean done

---
+++ index.js
@@ -0,0 +1,30 @@
+
+var myName = "Mirza Ayon";
+
+var age = "2.50";
+// myName = "Mirza Ayon";
+
+// age = "2.50"; 
+//chaile evabe var keyword charao likha jabe 
+console.log(age);
+console.log(myName);
+
+var myFriends = ["abc", "xyz"];
+console.log(myFriends);
+console.log(parseFloat(age));
+//white hole string and yellow hole number
+
+var num1 = 40.5;
+var num2 = "85";
+console.log(num1, num2);
+
+var isRaining = false;
+console.log(isRaining);
+
+
+var frndName;
+
+frndName = "someone";
+console.log(frndName)
+
+//number,string,bolean done
