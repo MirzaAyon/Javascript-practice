3def563af8cbf6f5c29e776db2181689a7063178
MirzaAyon <ayonmirza98@gmail.com>
2022-05-16 22:39:08 +0600

    for-loop added

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+var isRaining = false;
+var rainSpeed = "low";
+
+
+if (isRaining == true && rainSpeed == "high") {
+    console.log("vai , ami code korbo.ajke ber hobona .");
+} else if (isRaining == true || rainSpeed == "low") {
+    console.log("vai ektu dekhi . jodi kome jai ektu pore ber hobo.");
+}
+else {
+    console.log("vai , ber hobo ajke....");
+}
